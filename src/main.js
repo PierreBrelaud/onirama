dc038e2c45f75c6f13e1cf82c01abdd6b78057ea
LAPTOP-MJVBBP8Q\pierr <pierre.brelaud@inventhys.com>
2021-04-22 11:28:57 +0200

    ci: add vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'troisjs'
 import App from './App.vue'
+import router from 'vue-router'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
+
+
