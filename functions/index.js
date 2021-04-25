c530c8e0d5e96e438d0a4de09eae334bd181df69
PierreBrelaud <PierreBrelaud@users.noreply.github.com>
2021-04-25 22:05:40 +0200

    api endpoint for image to text

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const vision = require("@google-cloud/vision");
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({origin: true}));
+
+app.post("/imageToText", async (req, res) => {
+    const client = new vision.ImageAnnotatorClient();
+
+    const options = {
+        image: {
+            content: Buffer.from(req.body.base64Image, "base64"),
+        },
+    };
+
+    const [result] = await client.documentTextDetection(options);
+
+    res.send(result.fullTextAnnotation.text);
+});
+
+exports.api = functions.https.onRequest(app);
