d3311c071e149318378cd957428f290d7c0dfe09
PierreBrelaud <PierreBrelaud@users.noreply.github.com>
2021-04-25 16:57:04 +0200

    basic home buttons

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,11 +1,48 @@
 <template>
     <h1>Home</h1>
+
+    <div class="home__buttons">
+        <base-home-button
+            :icon="'1'" 
+            :action="btn1Clicked"
+        />
+        <base-home-button
+            :icon="'2'" 
+            :action="btn2Clicked"
+        />
+        <base-home-button
+            :icon="'3'" 
+            :action="btn3Clicked"
+        />
+    </div>
 </template>
 
 <script>
+import BaseHomeButton from '@/components/base/BaseHomeButton.vue'
+
 export default {
+    components: {
+        BaseHomeButton
+    },
+    methods: {
+        btn1Clicked() {
+            console.log('button 1 clicked')
+        },
+        btn2Clicked() {
+            console.log('button 2 clicked')
+        },
+        btn3Clicked() {
+            console.log('button 3 clicked')
+        },
+    }
 }
 </script>
 
-<style>
-</style> 
+<style lang="scss" scoped>
+.home__buttons {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
