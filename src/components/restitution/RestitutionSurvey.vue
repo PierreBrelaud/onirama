e4065466288203ce48b3b7a9de33b7e76958422c
LAPTOP-MJVBBP8Q\pierr <pierre.brelaud@inventhys.com>
2021-05-06 15:27:59 +0200

    select style

--- src/components/restitution/RestitutionSurvey.vue
+++ src/components/restitution/RestitutionSurvey.vue
@@ -159,7 +159,20 @@ export default {
 	align-items: center;
 
 	&__input {
-		margin-left: auto;
+        cursor: pointer;
+        margin-left: auto;
+        border: none;
+        font-size: 1.4rem;
+        color: gray;
+        padding-right: 2rem;
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        appearance: none;
+        background: transparent;
+        background-image: url("data:image/svg+xml;utf8,<svg fill='gray' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+        background-repeat: no-repeat;
+        background-position-x: 100%;
+        background-position-y: -.2rem;
 	}
 }
 
