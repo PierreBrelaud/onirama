b74b239316eff33b82169693551ff03a6a3be659
LAPTOP-MJVBBP8Q\pierr <pierre.brelaud@inventhys.com>
2021-06-03 18:36:39 +0200

    persist store state

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'; 
 import { loader } from '@/store/modules/loader'
 import { labeling } from '@/store/modules/labeling'
 import { restitution } from "@/store/modules/restitution";
 import { auth } from "@/store/modules/auth";
 
+
 const store = createStore({
+    plugins: [createPersistedState({
+        storage: window.sessionStorage,
+    })],
     modules: {
         loader,
         labeling,
