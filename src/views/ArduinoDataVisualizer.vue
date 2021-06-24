37a912aad75c8cce896357f8b16a5c20011a9f23
nicolasJupiter <nicolas.jupiter@pistil-studio.fr>
2021-06-24 09:25:50 +0200

    add graph for arduino data

--- src/views/ArduinoDataVisualizer.vue
+++ src/views/ArduinoDataVisualizer.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="dataVisualizer">
     <div class="dataVisualizer__text">{{stateText}}</div>
-    <div class="dataVisualizer__wrapper">
-      <div class="dataVisualizer__array">
-        <div class="dataVisualizer__array__title">Températures</div>
-        <div class="dataVisualizer__array__item" v-for="item in dataValues.temp">{{item}}</div>
-      </div>
-      <div class="dataVisualizer__array">
-        <div class="dataVisualizer__array__title">Cardiaques</div>
-        <div class="dataVisualizer__array__item" v-for="item in dataValues.cardiac">{{item}}</div>
-      </div>
+    <div class="dataVisualizer__chart">
+      <canvas id="arduino-chart"></canvas>
     </div>
   </div>
 </template>
 
 <script>
 import { db } from '@/firebase/index.js'
+import Chart from 'chart.js'
 
 export default {
   data() {
@@ -28,13 +22,18 @@ export default {
       dataValues: {
         "temp" : null,
         "cardiac": null
-      }
+      },
+      datacollection: null,
+      chart: null,
+      count: 0,
+      isOnce: true
     }
   },
   mounted() {
     this.$data.stateText = "En attente de données"
     this.checkDeviceState()
     this.getDeviceDatas()
+    this.createChart()
   },
   methods: {
     checkDeviceState() {
@@ -62,15 +61,70 @@ export default {
           .doc('temp')
           .onSnapshot((doc) => {
             if(doc.data() !== undefined) {
-              this.$data.dataValues.temp = doc.data().temperature
-              this.$data.dataValues.cardiac = doc.data().cardiac
+              if(doc.data().temperature !== undefined) {
+
+                this.$data.dataValues.temp = doc.data().temperature
+                this.$data.dataValues.cardiac = doc.data().cardiac
+
+                if(this.isOnce) {
+                  this.$data.dataValues.temp.forEach((item, index) => {
+                    this.chart.data.labels.push(index);
+                    this.count = index;
+                  })
+                  this.isOnce = false
+                }
+
+                this.chart.data.datasets[0].data = this.$data.dataValues.temp
+                this.chart.data.datasets[1].data = this.$data.dataValues.cardiac
+                this.chart.data.labels.push(this.count);
+                this.count++
+                this.chart.update()
+              }
             }
           })
-    }
-
-  },
-  components: {
-  },
+    },
+    createChart() {
+      const ctx = document.getElementById('arduino-chart');
+      ctx.height = 500;
+      this.chart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: [],
+          datasets: [
+            {
+              label: "Temperatures",
+              data: [],
+              backgroundColor: "rgba(54,73,93,.5)",
+              borderColor: "#36495d",
+              borderWidth: 3
+            },
+            {
+              label: "Cardiac",
+              data: [],
+              backgroundColor: "rgba(71, 183,132,.5)",
+              borderColor: "#47b784",
+              borderWidth: 3
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          lineTension: 1,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                 //  padding: 25
+                }
+              }
+            ]
+          }
+        }
+      });
+    },
+  }
 }
 </script>
 
@@ -80,10 +134,18 @@ export default {
   height: 100%;
   padding: 4rem;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   &__text {
     font-size: 5rem;
     color: $C-white;
     text-align: center;
+  }
+  &__chart {
+    canvas {
+      height: 100%;
+    }
   }
   &__wrapper {
     display: flex;
